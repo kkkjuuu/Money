f871ff6172c1f295a81697d498a010aa1fc4deff
kkkjuuu <101264768+kkkjuuu@users.noreply.github.com>
2026-07-11 02:54:18 +0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(express.static("public"));
+
+app.use("/api", require("./routes/transactions"));
+
+app.get("/health", (req, res) => {
+    res.json({
+        success: true,
+        message: "Saving API Running"
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server Started : ${PORT}`);
+});
