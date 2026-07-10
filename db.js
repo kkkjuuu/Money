1ddee15bbe9b42cf3c24ec92ab2883ebb827feba
kkkjuuu <101264768+kkkjuuu@users.noreply.github.com>
2026-07-11 02:54:36 +0700

    Create db.js

---
+++ db.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+
+const { Pool } = require("pg");
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+
+pool.on("connect", () => {
+    console.log("PostgreSQL Connected");
+});
+
+pool.on("error", (err) => {
+    console.error("Database Error :", err);
+});
+
+module.exports = pool;
